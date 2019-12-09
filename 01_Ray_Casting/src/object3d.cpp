7ab4261d066b3c48e3aecb1547311c12d526f4c3
zz sama <944707954@qq.com>
2019-12-09 12:49:11 +0800

    2019-12-09

--- 01_Ray_Casting/src/object3d.cpp
+++ 01_Ray_Casting/src/object3d.cpp
@@ -1,33 +1,61 @@
 #include "object3d.h"
 
 //Sphere
+//Algebraic
+//bool Sphere::intersect(const Ray &r, Hit &h, float tmin) {
+//    bool flag = false;
+//    Vec3f ro = r.getOrigin() - center;
+//    Vec3f rd = r.getDirection();
+//
+//    float a = rd.Dot3(rd);
+//    float b = 2 * ro.Dot3(rd);
+//    float c = ro.Dot3(ro) - radius * radius;
+//    float delta = b * b - 4 * a * c;
+//    float t = INFINITY;
+//    if (delta >= 0) {
+//        float d = sqrt(delta);
+//        float t1 = (-b - d) / (2 * a);
+//        float t2 = (-b + d) / (2 * a);
+//        if (t1 >= tmin) {
+//            t = t1;
+//        } else if (t2 >= tmin) {
+//            t = t2;
+//        }
+//        if (t < h.getT()) {
+//            h.set(t, material, r);
+//            flag = true;
+//        }
+//    }
+//    return flag;
+//}
+
+//Geometric
 bool Sphere::intersect(const Ray &r, Hit &h, float tmin) {
     bool flag = false;
     Vec3f ro = r.getOrigin() - center;
     Vec3f rd = r.getDirection();
-    //Algebraic
-    float a = rd.Dot3(rd);
-    float b = 2 * ro.Dot3(rd);
-    float c = ro.Dot3(ro) - radius * radius;
-    float delta = b * b - 4 * a * c;
-    float t = INFINITY;
-    if (delta >= 0) {
-        float d = sqrt(delta);
-        float t1 = (-b - d) / (2 * a);
-        float t2 = (-b + d) / (2 * a);
-        if (t1 >= tmin) {
-            t = t1;
-            flag = true;
-        } else if (t2 >= tmin) {
-            t = t2;
-            flag = true;
-        }
-        if (t < h.getT()) {
-            h.set(t, material, r);
-        }
+
+    float tp = -ro.Dot3(rd);
+    //if (tp < 0)
+    //    return false;
+    float d2 = ro.Dot3(ro) - tp * tp;
+    if (d2 > radius * radius)
+        return false;
+    float tt = sqrt(radius * radius - d2);
+    float t1 = tp - tt;  //out
+    float t2 = tp + tt;  //in
+    float t = t1;
+    //if(ro.Length() - radius>0)
+    //    t = t1;
+    //else
+    //    t = t2;
+    if (t > tmin && t < h.getT()) {
+        h.set(t, material, r);
+        flag = true;
     }
     return flag;
 }
+
 
 //Group
 bool Group::intersect(const Ray &r, Hit &h, float tmin) {
--- README.md
+++ README.md
@@ -20,9 +20,9 @@ http://groups.csail.mit.edu/graphics/classes/6.837/F04/index.html
 
 ### 1. Ray Casting
 
-这里使用 Algebraic 代数法计算交点，Geometric 再说。
+这里使用 Algebraic 和 Geometric 计算交点。
 
-注意点1：
+**注意点1：**不要 delete 同一片内存两次，不需要在 ~Object3D() 中 delete material; 因为 material 申请内存的操作不在这个类中，由其他类负责。
 
 ```
 class Object3D {
@@ -35,13 +35,13 @@ Material *material;
 //这个错误在clion中只会返回异常: 0xC0000005，而在vs中会指向错误的地方，当clion中返回值!=0时，又找不到出错的地方，可以在vs中找。
 ```
 
-注意点2：
+**注意点2：**
 
 image 对应于 （0,200）and 比例（0,1）
 
 OrthographicCamera 的 center 在（0,0），因此产生的光线对应于 （-1,1）* size
 
-注意点3：
+**注意点3：**
 
 error: multiple definition of 函数多次定义
 
@@ -49,7 +49,7 @@ error: multiple definition of 函数多次定义
 
 解决办法二：在 .h文件中，把定义与声明写在一起，即把函数定义写在类内。
 
-
+**注意点4：**在使用 Geometric 法计算交点时，为了得到与样例相符的图片，忽略 t 是否在视线后面，即 camera center 是否在球体内部，永远取 t = min(t1, t2)
 
 参考：
 

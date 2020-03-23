688f501481266beff6847d9a79ef9825fbc0fdea
fuzhanzhan <fuzhanzhan@foxmail.com>
2020-03-23 21:50:59 +0800

    Finish

--- Assignment9/rungeKutta.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "integrator.h"
-
-class RungeKuttaIntegrator :public Integrator {
-	pub
-};
--- Assignment9/rungeKuttaIntegrator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "integrator.h"
-
-class RungeKuttaIntegrator :public Integrator {
-public:
-	RungeKuttaIntegrator();
-	virtual ~RungeKuttaIntegrator();
-	virtual void Update(Particle* particle, ForceField* forcefield, float t, float dt) override;
-};
